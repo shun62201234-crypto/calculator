4f142b64314ee28d3b255e4e34025cf8c1a6d7e5
shun62201234-crypto <shun.622.01234@gmail.com>
2026-05-25 05:58:54 +0900

    Create Evaluator.ts

---
+++ src/domain/Evaluator.ts
@@ -0,0 +1,46 @@
+import { Operation } from "./Operation";
+import { DivisionByZeroError } from "../utility/DivisionByZeroError";
+
+/**
+ * 四則演算を行うクラス
+ * 演算子に基づいて計算結果を返す静的メソッドを提供
+ */
+export class Evaluator {
+    /**
+     * 2つの数値に対して指定された演算を実行する関数
+     * 
+     * @param a 左辺の値
+     * @param operator 使用する演算子
+     * @param b 右辺の値
+     * @returns 計算結果
+     * @throws DivisionByZeroError 0で割る操作が行われた時に発生
+     */
+    public static compute (a: number, operator: Operation, b: number): number {
+        switch (operator) {
+            case Operation.Add:
+                return a + b;
+
+            case Operation.Subtract:
+                return a - b;
+        
+            case Operation.Multiply:
+                return a * b;
+
+            case Operation.Divide:
+                if (b === 0){
+                    throw new DivisionByZeroError();
+                }
+                return a / b;
+        }
+    }
+}
+
+
+/**memo
+---
+defaultについては、Operationで演算子を定義しているので不要。
+演算子記号以外のその他記号が入ることがないため
+---
+default:
+    throw new Error("不明な演算子です");
+ */
