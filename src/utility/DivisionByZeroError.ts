72f3549bd855ddff08277c865c64878015fb25e0
shun62201234-crypto <shun.622.01234@gmail.com>
2026-05-25 05:59:52 +0900

    Create DivisionByZeroError.ts

---
+++ src/utility/DivisionByZeroError.ts
@@ -0,0 +1,27 @@
+
+export class DivisionByZeroError extends Error {
+    constructor() {
+        super("0で割れません");
+        this.name = "DivisionByZeroError";
+
+        Object.setPrototypeOf(this, DivisionByZeroError.prototype);
+    }
+}
+
+try {
+    throw new DivisionByZeroError();
+} catch (error: unknown) {
+    if (error instanceof DivisionByZeroError) {
+        console.log(error.name);
+        console.log(error.message);
+    }
+}
+
+
+
+// export class DivisionByZeroError extends Error {
+//     constructor(message: string = "0で割ることはできません") {
+//         super(message);
+//         this.name = "DivisionByZeroError";
+//     }
+// }
