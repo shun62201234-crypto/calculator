b3ac7050668490b243c9a9dff18adee34dcc5b31
shun62201234-crypto <shun.622.01234@gmail.com>
2026-05-29 05:22:08 +0900

    Update Operation.test.ts

--- src/test/Operation.test.ts
+++ src/test/Operation.test.ts
@@ -20,35 +20,3 @@ describe("Operation", () => {
     });
 });
 
-
-
-
-/**memo
----
-テストの基本形
-describe("〜", () => {
-    it("〜", () => {
-    expect(〜).toBe(〜);
-    });
-})
-describe = テストのグループ
-it       = テストケース（1つの検証）
-expect   = 実際のチェック
----
-①import
-import { describe, it, expect } from "vitest"; //describe / it / expect → テスト用関数
-import { Operation } from "../domain/Operation"; // peration → テスト対象
-
-②describe
-describe("Operation enum", () => { 
-テストのグループ化 「Operation enumについてのテスト」
-
-③it（テストケース）
-it("値が正しい", () => {
-1つの検証　「値が正しいことを確認する」
-
-④expect（ここが本体）
-expect(値).toBe(期待値)
-expect(Operation.Add).toBe("ADD");　
-// Operation.Add は "ADD" であるべき。　Operation.Add === "ADD"をチェックしている
-*/
